3c4635500f64a3c7b867e196f930342e323af9bc
Fabricio Padua <fabricio_0505_@hotmail.com>
2024-12-20 14:08:17 -0500

    Initial Commit

---
+++ MVVM-SwiftUI-Template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM-SwiftUI-Template
+//
+//  Created by Fabricio Padua on 12/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVM-SwiftUI-Template/MVVM_SwiftUI_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUI_TemplateApp.swift
+//  MVVM-SwiftUI-Template
+//
+//  Created by Fabricio Padua on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUI_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
